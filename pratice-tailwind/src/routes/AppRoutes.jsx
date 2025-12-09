2c3726978547456db99039431bbca2ff60093966
ezekiel <favourtobiloba200@gmail.com>
2025-12-09 12:47:08 +0100

    all files

--- pratice-tailwind/src/routes/AppRoutes.jsx
+++ pratice-tailwind/src/routes/AppRoutes.jsx
@@ -1,8 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "../Pages/Login";
-import Dashboard from "./../pages/Dashboard";
-import Profile from "./../pages/Profile";
-import Settings from "./../pages/Settings";
+import Dashboard from "../Pages/Dashboard";
+import Profile from "../Pages/Profile";
+import Settings from "../Pages/Settings";
 import ProtectedRoute from "./ProtectedRoute";
 
 export const AppRoutes = () => {
